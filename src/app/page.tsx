f0dc7ac558e1b2004dc9315e45f0e4da3d87c3d4
IraMira333 <irinat333@gmail.com>
2024-06-11 01:01:02 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Slide1 } from "@/components/Slide1";
+import { Slide2 } from "@/components/Slide2";
+import { Slide3 } from "@/components/Slide3";
+import { Slide4 } from "@/components/Slide4";
 
 export default function Home() {
     return (
         <>
             <Slide1 />
+            <Slide2 />
+            <Slide3 />
+            <Slide4 />
         </>
     );
 }
