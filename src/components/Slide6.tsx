fa50c8fb500f45beefbc63f4baab276a1dab0e76
IraMira333 <irinat333@gmail.com>
2024-06-12 23:52:21 +0300

    Create Slide6.tsx

---
+++ src/components/Slide6.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { FAQItem } from "./FAQItem";
+import { FAQData } from "@/mockedData/FAQData";
+
+export const Slide6 = () => {
+    return (
+        <section className="p-5">
+            <div className="max-w-[500px] mx-auto">
+                <div className="flex justify-between mb-7">
+                    <div className="my-auto w-[35%]">
+                        <Image
+                            src="/cameraSmall.png"
+                            alt="Camera small"
+                            width={370}
+                            height={455}
+                            className="scale-x-[-1]"
+                        />
+                    </div>
+                    <h2 className=" uppercase font-notoDisplay text-2xl w-[60%] my-auto text-right">
+                        Питання та Відповіді
+                    </h2>
+                </div>
+                <ul>
+                    {FAQData.map(faq => (
+                        <li
+                            key={faq.id}
+                            className="relative pb-3 pl-4 border-l"
+                        >
+                            <FAQItem
+                                question={faq.question}
+                                answer={faq.answer}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+};
