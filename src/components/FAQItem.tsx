e7da91cc7643d5f24ae68395010c49bc0013f35d
IraMira333 <irinat333@gmail.com>
2024-06-12 23:51:58 +0300

    Create FAQItem.tsx

---
+++ src/components/FAQItem.tsx
@@ -0,0 +1,16 @@
+import { IconReel } from "./IconReel";
+
+type FAQDataItemType = {
+    question: string;
+    answer: string;
+};
+export const FAQItem = ({ question, answer }: FAQDataItemType) => {
+    return (
+        <>
+            <IconReel />
+            <h4 className="italic text-lg mb-2 pl-3">{question}</h4>
+
+            <p className="mb-5">{answer}</p>
+        </>
+    );
+};
