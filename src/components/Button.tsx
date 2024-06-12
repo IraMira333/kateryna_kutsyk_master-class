50b73e505b5ed51aed0e3f3ff1484978ab40172e
IraMira333 <irinat333@gmail.com>
2024-06-12 23:51:55 +0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export const Button = ({ type }: ButtonProps) => {
         <button
             type={type}
             onClick={() => alert(process.env.NEXT_PUBLIC_MESSAGE)}
-            className="px-2 py-1 bg-button rounded-sm active:bg-accentBg active:text-accentText"
+            className="px-2 py-1 bg-button border border-accentText rounded-sm active:bg-accentBg active:text-accentText transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
         >
             отримати доступ
         </button>
