89d554923f002201257a7fd0276a4d5b0fbee771
IraMira333 <irinat333@gmail.com>
2024-06-12 23:52:30 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Slide1 } from "@/components/Slide1";
 import { Slide2 } from "@/components/Slide2";
 import { Slide3 } from "@/components/Slide3";
 import { Slide4 } from "@/components/Slide4";
+import { Slide5 } from "@/components/Slide5";
+import { Slide6 } from "@/components/Slide6";
+import { Slide7 } from "@/components/Slide7";
 
 export default function Home() {
     return (
@@ -10,6 +13,9 @@ export default function Home() {
             <Slide2 />
             <Slide3 />
             <Slide4 />
+            <Slide5 />
+            <Slide6 />
+            <Slide7 />
         </>
     );
 }
