174b6bbe37109765ad88782a9aa1b545a6359e09
IraMira333 <irinat333@gmail.com>
2024-06-14 21:36:43 +0300

    Added photo

--- src/components/Slide3.tsx
+++ src/components/Slide3.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
 export const Slide3 = () => {
     return (
         <section className="p-5 text-justify">
             <div className="max-w-[500px] mx-auto">
-                <div></div>
+                <div className="w-[90%] mx-auto mb-7 bg-shading ">
+                    <Image
+                        src="https://drive.google.com/uc?export=view&id=1VBR7B1mKoFjn5O3KlbBoKVylxl_AOm-O"
+                        alt="Kateryna's Nosatuyk photo"
+                        width={849}
+                        height={1280}
+                        className="relative z-[-1]"
+                    />
+                </div>
                 <h2 className="italic text-lg text-center mb-5">
                     Вітаю! Я, Катерина Носатюк,
                 </h2>
