acef7ff716b0edfd5a5ecb4a0444f6b36fb59180
IraMira333 <irinat333@gmail.com>
2024-06-19 22:52:52 +0300

    Renamed bgImage and added priority

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,10 +6,11 @@ export const Header = () => {
         <header className="fixed top-0 left-0 w-full z-50">
             <div className=" h-[80px]  bg-shading w-[100vw]">
                 <Image
-                    src="/bgSlide7.png"
+                    src="/bgHeader.png"
                     alt="Background image"
                     width={1908}
                     height={464}
+                    priority
                     className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
                 />
                 <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-full flex justify-between max-w-[500px] px-4 items-center">
--- src/components/Slide7/Slide7.tsx
+++ src/components/Slide7/Slide7.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { FormGetCourse } from "../shared/FormGetCourse";
 
 export const Slide7 = () => {
