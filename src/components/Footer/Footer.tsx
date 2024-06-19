224a5de02c6c1dd222d3b9a0d4f9639d0be10f69
IraMira333 <irinat333@gmail.com>
2024-06-20 01:46:47 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,15 +3,6 @@ export const Footer = () => {
         <footer className="py-5 text-center">
             <div className="p-4">
                 <div className="m-5">
-                    <p>ФОП Носатюк Катерина Леонідівна </p>
-                    <p>UA283220010000026000001389792</p>
-                    <p>МФО 322001,</p>
-                    <p className="mb-3">
-                        ЄДРПОУ 21133352 в АТ “УНІВЕРСАЛ БАНК”
-                    </p>
-                    <p className="mb-1">
-                        04210, Україна, м.Київ, просп. В.Івасюка 6а
-                    </p>
                     <div className="mb-1 active:text-accentText transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]">
                         <a href="tel:+380933625757" className="">
                             +38 (093) 362-57-57
