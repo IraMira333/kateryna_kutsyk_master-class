8fd143caddec3c25741d68ad4f5dcf12e918a197
IraMira333 <irinat333@gmail.com>
2024-06-19 22:50:51 +0300

    Create Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Button } from "../shared/Button";
+
+export const Header = () => {
+    return (
+        <header className="fixed top-0 left-0 w-full z-50">
+            <div className=" h-[80px]  bg-shading w-[100vw]">
+                <Image
+                    src="/bgSlide7.png"
+                    alt="Background image"
+                    width={1908}
+                    height={464}
+                    className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
+                />
+                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-full flex justify-between max-w-[500px] px-4 items-center">
+                    <Image
+                        src="/freeform.png"
+                        alt="Background image"
+                        width={238}
+                        height={216}
+                        className="w-[70px] h-auto"
+                    />
+                    <div className=" my-auto">
+                        <Button type="button" />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
