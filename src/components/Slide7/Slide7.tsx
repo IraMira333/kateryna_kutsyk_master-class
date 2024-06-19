0b2b2613e57ae88018efeb9b5f7455cd69a34b46
IraMira333 <irinat333@gmail.com>
2024-06-19 22:41:29 +0300

    Update Slide7.tsx

--- src/components/Slide7/Slide7.tsx
+++ src/components/Slide7/Slide7.tsx
@@ -4,15 +4,7 @@ import { FormGetCourse } from "../shared/FormGetCourse";
 export const Slide7 = () => {
     return (
         <section className="relative py-5 text-center">
-            <div className=" bg-shading mb-5 w-[100vw]">
-                <Image
-                    src="/bgSlide7.png"
-                    alt="Background image"
-                    width={1908}
-                    height={464}
-                    className="relative z-[-1] min-h-[220px] w-full object-left-top object-cover"
-                />
-            </div>
+            <div className=" bg-coursesGradient mb-5 w-[100vw] h-[220px]"></div>
             <div className="px-5 max-w-[500px] mx-auto mb-12">
                 <div className="absolute top-12 left-1/2 transform -translate-x-1/2 w-full max-w-[95%] ">
                     <h2 className="  uppercase font-notoDisplay text-3xl text-center">
