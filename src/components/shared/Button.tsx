9b91fcbaf25b0902fe9d2b309182acb73a6f3eb9
IraMira333 <irinat333@gmail.com>
2024-06-21 00:13:05 +0300

    Added Link to courses

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -5,12 +5,16 @@ type ButtonProps = {
 
 export const Button = ({ type }: ButtonProps) => {
     return (
-        <button
-            type={type}
-            onClick={() => alert(process.env.NEXT_PUBLIC_MESSAGE)}
-            className="px-2 py-1 bg-button border border-accentText rounded-sm active:bg-accentBg active:text-accentText transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
+        <a
+            target="_blank"
+            href="https://katerynanosatuyk.lms.softbook.app/shop/item/6580"
         >
-            отримати доступ
-        </button>
+            <button
+                type={type}
+                className="px-2 py-1 bg-button border border-accentText rounded-sm active:bg-accentBg active:text-accentText transition-all duration-[250ms] ease-[cubic-bezier(0.4,0,0.2,1)]"
+            >
+                отримати доступ
+            </button>
+        </a>
     );
 };
