164fe1aaaa95bedb18ada0a820805620d4015b6a
IraMira333 <irinat333@gmail.com>
2024-06-20 22:58:07 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import { Button } from "../shared/Button";
+import Link from "next/link";
 
 export const Header = () => {
     return (
         <header className="fixed top-0 left-0 w-full z-50">
             <div className=" h-[80px]  bg-shading w-[100vw]">
-                <Image
-                    src="/bgHeader.png"
-                    alt="Background image"
-                    width={477}
-                    height={116}
-                    priority
-                    className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
-                />
+                <Link href="/">
+                    <Image
+                        src="/bgHeader.png"
+                        alt="Background image"
+                        width={477}
+                        height={116}
+                        priority
+                        className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
+                    />
+                </Link>
                 <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-full flex justify-between max-w-[500px] px-4 items-center">
                     <Image
                         src="/freeform.png"
