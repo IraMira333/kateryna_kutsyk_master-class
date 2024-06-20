97c07cff2797273db391b2a033b91056a0a6ecb6
IraMira333 <irinat333@gmail.com>
2024-06-20 23:01:16 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,24 +6,25 @@ export const Header = () => {
     return (
         <header className="fixed top-0 left-0 w-full z-50">
             <div className=" h-[80px]  bg-shading w-[100vw]">
-                <Link href="/">
-                    <Image
-                        src="/bgHeader.png"
-                        alt="Background image"
-                        width={477}
-                        height={116}
-                        priority
-                        className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
-                    />
-                </Link>
+                <Image
+                    src="/bgHeader.png"
+                    alt="Background image"
+                    width={477}
+                    height={116}
+                    priority
+                    className="relative z-[-1] h-[80px] w-full object-left-top object-cover"
+                />
+
                 <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-full flex justify-between max-w-[500px] px-4 items-center">
-                    <Image
-                        src="/freeform.png"
-                        alt="Background image"
-                        width={238}
-                        height={216}
-                        className="w-[70px] h-auto"
-                    />
+                    <Link href="/">
+                        <Image
+                            src="/freeform.png"
+                            alt="Background image"
+                            width={238}
+                            height={216}
+                            className="w-[70px] h-auto"
+                        />
+                    </Link>
                     <div className=" my-auto">
                         <Button type="button" />
                     </div>
